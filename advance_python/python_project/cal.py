0ec6eb6d4c0c1f7ee1f7b763322ecdd78744bc7d
Hariom <hariom14may11@gmail.com>
2020-07-28 02:12:56 +0530

    module files added

---
+++ advance_python/python_project/cal.py
@@ -0,0 +1,32 @@
+# this  is module file of calcuation
+#
+
+var = 50
+
+
+def name():
+    print('cal module variable:', var)
+
+
+def add(a, b):
+    return (a+b)
+
+
+def sub(a, b):
+    return (a-b)
+
+
+def mul(a, b):
+    return (a*b)
+
+
+def div(a, b):
+    return(a/b)
+
+
+def square(a):
+    return (a*a)
+
+
+def cube(a):
+    return(a*a*a)
---
+++ advance_python/python_project/main.py
@@ -0,0 +1,36 @@
+# this is main project file
+# here we will import of module files
+
+
+'''
+# this is import module_name type
+
+import cal
+
+
+cal.name()
+sub = cal.sub(10, 5)
+print("the addidion is ", cal.add(5, 10))
+print("the subtraction is ", sub)
+mul = cal.mul
+print("The miltiplication is ", mul(5, 10))
+d = cal.div
+div = d(10, 5)
+print("the division  is ", div)
+print("the square is:", cal.square(5))
+print("the cube is:", cal.cube(5))
+
+'''
+
+'''
+
+# this is import module_name  as alias_name type
+
+import cal as c
+
+c.name()  # it will print the function name()
+add = c.add(5, 10)
+print("The addition is ", add)
+print("The substraction is ", c.sub(10, 5))
+
+'''
